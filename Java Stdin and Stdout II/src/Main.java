12d555bdecdf1ee870eb91f95f660613364c0918
dawid <dawidgardzi@onet.pl>
2018-12-17 15:20:08 +0100

    java 3

--- Java Stdin and Stdout II/src/Main.java
+++ Java Stdin and Stdout II/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scan = new Scanner(System.in);
+        int numberInt = scan.nextInt();
+        double numberDouble = scan.nextDouble();
+        scan.nextLine();
+        String text = scan.nextLine();
+        scan.close();
+
+
+
+        System.out.println("String: " + text);
+        System.out.println("Double: " + numberDouble);
+        System.out.println("Int: " + numberInt);
+
     }
 }
